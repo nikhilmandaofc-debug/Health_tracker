94ed62c6ce982a5e671c77e2d94b10203212fef0
nikhilmandaofc-debug <nikhilmandaofc@gmail.com>
2026-03-04 15:15:25 -0600

    Update ai_rule_service.py

--- backend/app/services/ai_rule_service.py
+++ backend/app/services/ai_rule_service.py
@@ -2,10 +2,19 @@ import os
 import json
 from dotenv import load_dotenv
 from openai import OpenAI
+from app.models.ai_models import  (
+    deepseek_r1,
+    gpt4o,
+    gpt4o_mini,
+    deepseek_v3,
+    llama_33,
+    embeddings_model
+)
+import re
 
 load_dotenv()
 
-client = OpenAI(api_key="1234")
+client = OpenAI(api_key="sk-qXX8aIa3K4bjW8pkHU5SlQ")
 
 
 def generate_rule_from_prompt(prompt: str):
@@ -21,7 +30,7 @@ parameter (oxygen_level | heart_rate | temperature)
 operator (< or >)
 threshold (number)
 category (Critical | Moderate | stable)
-ward (ICU | ER | General)
+ward (ICU | ER | OP)
 
 Return ONLY JSON.
 
@@ -36,17 +45,25 @@ Example:
 }
 """
 
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
+    # response = client.chat.completions.create(
+    #     model="gpt-4o-mini",
+    #     messages=[
+    #         {"role": "system", "content": system_prompt},
+    #         {"role": "user", "content": prompt}
+    #     ]
+    # )
+
+    messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": prompt}
         ]
-    )
+    
+    response = deepseek_v3.invoke(messages)
 
-    result = response.choices[0].message.content
-
+    result = response.content
+    print(result)
     try:
-        return json.loads(result)
+        cleaned = re.sub(r"```json|```", "", result).strip()
+        return json.loads(cleaned)
     except:
         raise Exception("AI returned invalid JSON")
